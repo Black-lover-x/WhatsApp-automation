5cf8d2fe0bde8cc4758bfc6c0d02411daca04725
Patel Suraj <96813753+Black-lover-x@users.noreply.github.com>
2022-05-16 08:26:51 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+#whatsapp message with python
+import pyautogui as ss
+
+import time as t
+
+t.sleep(10)
+
+for i in range(100):
+	ss.write('Hi FRIENDS')
+	ss.press('Enter')
+	
+#write by @suraj_patel_official	
+	
+	
